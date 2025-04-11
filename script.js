edaf644bbe776e635043e3ea055c557d26136c81
Luciano Medici <lucianomedici100k@gmail.com>
2025-04-11 19:56:08 -0300

    create basic game functionality without styles
    The game can now be easily played through the DOM,
    not the console as it was previously. First basic
    UI created.

---
+++ script.js
@@ -0,0 +1,65 @@
+function getComputerChoice(){
+    let randomNumber = Math.ceil(Math.random() * 3);
+    if (randomNumber == 1) return "rock";
+    if (randomNumber == 2) return "scissors";
+        return "paper";
+}
+
+function playRound(humanChoice) {
+    computerChoice = getComputerChoice();
+    playerSide.textContent = "";
+    playerSide.textContent = "Player choose: " + humanChoice;
+    computerSide.textContent = "";
+    computerSide.textContent = "Computer choice: " + computerChoice;
+    if (humanChoice === computerChoice){
+        score.textContent = `Tie! Nobody win this round\n\rPlayer = ${playerScore}\n\rComputer = ${computerScore}`;
+    } else if (humanChoice === "rock" && computerChoice === "scissors" ||
+    humanChoice === "paper" && computerChoice === "rock" ||
+    humanChoice === "scissors" && computerChoice === "paper"){
+        playerScore++;
+        score.textContent = `Player wins this round\n\rPlayer = ${playerScore}\n\rComputer = ${computerScore}`;
+    } else {
+        computerScore++;
+        score.textContent = `Computer wins this round\n\rPlayer = ${playerScore}\n\rComputer = ${computerScore}`;
+    }
+    if (playerScore == 5){
+        playerScore = 0;
+        computerScore = 0;
+        score.textContent = "";
+        score.textContent = "Player wins the GAME!. Select an option to start playing again.";
+    } else if (computerScore == 5) {
+        playerScore = 0;
+        computerScore = 0;
+        score.textContent = "";
+        score.textContent = "Computer wins the GAME!. Select an option to start playing again.";
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+const playerSide = document.createElement("div");
+const computerSide = document.createElement("div");
+const score = document.createElement("p");
+const options = document.querySelector("#options");
+options.appendChild(playerSide);
+options.appendChild(computerSide);
+options.appendChild(score);
+
+
+
+const playerSelection = document.querySelector("#options");
+playerSelection.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch(target.id){
+        case "rock":
+            playRound("rock");
+            break;
+        case "scissors":
+            playRound("scissors");
+            break;
+        case "paper":
+            playRound("paper");
+            break;
+    }
+});
